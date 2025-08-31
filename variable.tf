5edfa14ac095d57a783ced3a69ffb20466660561
aravind <ias1.email@example.com>
2025-08-31 13:09:28 +0530

    vnet

--- variable.tf
+++ variable.tf
@@ -8,3 +8,12 @@ variable "location" {
   default = "East US"
   
 }
+variable "vnet_name" {
+  type = string
+  default = "arav-vnet"
+  
+}
+variable "address_space" {
+  type = string
+  default = "10.0.0.0/16"
+   }
